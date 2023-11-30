a80f7d5592061e159a095748d765e874146d6f29
Diego Izaguirre <tenochca.di@outlook.com>
2023-11-30 01:23:52 -0600

    set up function to get json reponse from api. Need to parse the response. Also set up a mapping that corresponds to the route codes for the api endpoint.

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import org.json.*;
+import java.io.*;
+
+
+public class Main {
+    public static void main(String[] args) throws JSONException, IOException {
+        TrainTracker.getResponse("Green Line");
+    }
+}
---
+++ src/TrainTracker.java
@@ -0,0 +1,49 @@
+import org.json.*;
+import java.io.*;
+import java.net.*;
+import java.util.HashMap;
+import java.util.Map;
+public class TrainTracker {
+
+    static Map<String, String> trainRoute = new HashMap<>()  {{
+        put("Red Line", "Red");
+        put("Blue Line", "Blue");
+        put("Brown Line", "Brn");
+        put("Green Line", "G");
+        put("Orange Line", "Org");
+        put("Purple line", "P");
+        put("Pink Line", "Pink");
+        put("Skokie Swift", "Y");
+    }};
+
+    //api endpoint for getting train information
+    public static final String BASE_URL = "https://lapi.transitchicago.com/api/1.0/ttpositions.aspx?key=";
+    public static final String API_KEY = "57ab1165b4644d7a90321c937243a2b8";
+
+    public static void getResponse(String trainLine) throws IOException, JSONException {
+        String API_URL = BASE_URL + API_KEY + "&outputType=JSON" + "&rt=" + trainRoute.get(trainLine);
+
+        URL url = new URL(API_URL);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection(); //opens a connection for the given url
+        connection.setRequestMethod("GET"); //sets the method, in this case we only want to get info
+        int responseCode = connection.getResponseCode();
+
+        if (responseCode == HttpURLConnection.HTTP_OK) { //checking if the request was a success
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream())); //storing the response data
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) { //storing the readLine in inputLine while that line != null
+                response.append(inputLine); //append that line to response
+
+            }
+            //closing the buffered reader
+            in.close();
+            System.out.println(response.toString());
+
+            //parse JSON
+            JSONObject jsonResponse = new JSONObject(response.toString());
+
+        }
+    }
+}
